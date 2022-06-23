058e8419ea0145d166e797377eda1ad90e22ff79
Maximilian Pfitzenmaier <max.pfitzenmaier@web.de>
2022-06-23 23:55:14 +0200

    fixed: component not found

--- app/src/index.js
+++ app/src/index.js
@@ -2,7 +2,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App';
 import Index from './components/Index';
-import NotFound from './Components/NotFound';
+import NotFound from './components/NotFound';
 
 //const el = document.getElementById('root');
 const root = ReactDOM.createRoot(document.getElementById('root'));
